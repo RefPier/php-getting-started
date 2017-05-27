acf744384afcc6e5795469c12591ad2135035878
RefPier <pier.mn@hotmail.it>
2017-05-27 11:54:25 +0200

    fix

--- web/phpScripts/Config.php
+++ web/phpScripts/Config.php
@@ -1,16 +1,9 @@
-<?php 
-$url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
+<?php
 
-$hostname = $dbparts['host'];
-$username = $dbparts['user'];
-$password = $dbparts['pass'];
-$database = ltrim($dbparts['path'],'/');
-
- define('DB_USERNAME',$username);
- define('DB_PASSWORD',$password);
- define('DB_NAME',$hostname);
- define('DB_HOST',$database);
+ define('DB_USERNAME','odiq7bb07px35p46');
+ define('DB_PASSWORD','d8szosqai45b0t3d');
+ define('DB_NAME','sg8mpl3g3crxg3yp');
+ define('DB_HOST','thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
 
 //defined a new constant for firebase api key
  define('FIREBASE_API_KEY', 'AAAAEFPbhLQ:APA91bF5gGM-3_JKeDC1y-yxJB9yvpqGNONLn0MZj6TPpCwjio39swUbtju84Jq5_glh1iBq6waPMhJlvbVVnYn_JHTEaC0YVlUsYkH9Ld-gjNbleBGfVb_7zr0IYqfcr0kzGBlcbVNy');
